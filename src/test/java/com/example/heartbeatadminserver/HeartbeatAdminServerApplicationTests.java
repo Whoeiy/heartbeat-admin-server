677a7a66600dba5f6ea2ae1e66cbb3a359e91101
lhb <929580807@qq.com>
2022-05-28 12:38:09 +0800

    lhb self-push try 3

--- src/test/java/com/example/heartbeatadminserver/HeartbeatAdminServerApplicationTests.java
+++ src/test/java/com/example/heartbeatadminserver/HeartbeatAdminServerApplicationTests.java
@@ -1,16 +1,75 @@
 package com.example.heartbeatadminserver;
 
 import com.example.heartbeatadminserver.dao.AdminDao;
+import com.example.heartbeatadminserver.dao.CategoryDao;
 import com.example.heartbeatadminserver.dao.VendorDao;
 import com.example.heartbeatadminserver.entity.Admin;
 import com.example.heartbeatadminserver.entity.AdminToken;
+import com.example.heartbeatadminserver.entity.Category;
 import com.example.heartbeatadminserver.entity.Vendor;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Date;
+
 @SpringBootTest
 class HeartbeatAdminServerApplicationTests {
+
+    // category dao test
+    // 数据层测试
+    @Autowired
+    private CategoryDao categoryDao;
+
+    @Test
+    void testGetById(){
+        System.out.println(categoryDao.selectById(1));
+    }
+
+//    @Test
+//    void testSave(){
+//        Category category = new Category();
+//        category.setCategoryname("test2");
+//        category.setCategorylevel(2);
+//        category.setCategoryname("test2");
+//        category.setCategoryrank(2);
+//        category.setParentid(2);
+//        category.setCreateuser(2);
+//        System.out.println(category);
+//        categoryDao.insert(category);
+//    }
+
+//
+//    @Test
+//    void testUpdate(){
+//        System.out.println(categoryDao.selectById());
+//    }
+//
+//    @Test
+//    void testDelete(){
+//        System.out.println(categoryDao.selectById());
+//    }
+//
+//    @Test
+//    void testGetAll(){
+//        System.out.println(categoryDao.selectById());
+//    }
+//
+//    @Test
+//    void testGetPage(){
+//        System.out.println(categoryDao.selectById());
+//    }
+//
+//    @Test
+//    void testGetBy(){
+//        System.out.println(categoryDao.selectById());
+//    }
+
+
+
+
+
+
 
     @Autowired
     private AdminDao adminDao;
