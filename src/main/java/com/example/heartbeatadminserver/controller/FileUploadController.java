774a56919f6e91c8e0b48230eefaea8dc0826760
yy <jenniferyue@outlook.com>
2022-06-03 22:40:58 +0800

    yy: 文件上传功能

--- src/main/java/com/example/heartbeatadminserver/controller/FileUploadController.java
+++ src/main/java/com/example/heartbeatadminserver/controller/FileUploadController.java
@@ -1,48 +1,120 @@
 package com.example.heartbeatadminserver.controller;
 
+import com.example.heartbeatadminserver.util.HeartBeatUtils;
+import com.example.heartbeatadminserver.util.Result;
+import com.example.heartbeatadminserver.util.ResultGenerator;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+import org.springframework.web.multipart.commons.CommonsMultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
+import java.util.*;
 
+@Slf4j
 @RestController
+@RequestMapping("/admin/upload")
 public class FileUploadController {
 
     SimpleDateFormat sdf = new SimpleDateFormat("/yyyy/MM/dd");
+    @Value("${file.server_path}")   // 服务器地址
+//    @Value("${file.local_path}")
+    private String path;
 
-    @PostMapping("/admin/upload")
-    public Map<String, Object> fileupload(MultipartFile file, HttpServletRequest req) {
-        Map<String, Object> result = new HashMap<>();
-        String originalFilename = file.getOriginalFilename();
-        if (!originalFilename.endsWith(".png")) {
-            result.put("status", "error");
-            result.put("msg", "文件类型需为png后结尾");
-            return result;
+    @PostMapping("/file")
+    @ApiOperation("单个文件上传")
+    public Result uploadFile(HttpServletRequest req, @RequestParam("file") MultipartFile file, int adminId) {
+        if (file.isEmpty()) {
+            return ResultGenerator.genFailResult("文件不能为空");
         }
-        String format = sdf.format(new Date());
-        String realpath = req.getServletContext().getRealPath("/") + format;
-        File folder = new File(realpath);
-        if(!folder.exists()){
-            folder.mkdir();
+        String originalName = file.getOriginalFilename();
+        File file1 = new File(path);
+        if (!file1.exists()) {
+            file1.mkdirs();
         }
-        String newName = UUID.randomUUID().toString() + ".png";
+        String fileName = UUID.randomUUID().toString() + originalName.substring(originalName.lastIndexOf("."));
+        File dest = new File(path, fileName);
+        String temp = "";
         try {
-            file.transferTo(new File(folder,newName));
-            String url = req.getScheme() + "://" + req.getServerName() + ":" + req.getServerPort() + format + newName;
-            result.put("status","success");
-            result.put("url",url);
-        } catch (IOException e) {
-            result.put("status", "error");
-            result.put("msg", e.getMessage());
+            file.transferTo(dest);
+            temp += HeartBeatUtils.getHost(new URI(req.getRequestURL() + "")) + "/upload/" + fileName;
+        } catch (IOException | URISyntaxException e) {
+            e.printStackTrace();
+            return ResultGenerator.genFailResult("上传文件失败");
         }
-        return result;
+        return ResultGenerator.genSuccessResultData(temp);
     }
+    @PostMapping("/files")
+    @ApiOperation("多个文件上传")
+    public Result uploadFiles(HttpServletRequest req, HttpServletResponse res, int adminId) {
+        List<MultipartFile> files = ((MultipartHttpServletRequest) req).getFiles("file");
+        if( files == null || files.size() == 0) {
+            return ResultGenerator.genFailResult("文件不能为空");
+        }
+        File file1 = new File(path);
+        String temp = "";
+        if (!file1.exists()) {
+            file1.mkdirs();
+        }
+        for (int i = 0; i < files.size(); i++) {
+            MultipartFile file = files.get(i);
+            String originalName = file.getOriginalFilename();
+            if (!(originalName.endsWith(".png") || originalName.endsWith(".jpeg") || originalName.endsWith(".jpg"))){
+                return ResultGenerator.genFailResult("上传第" + i + "个文件失败," + "文件格式不支持");
+            }
+            if(file.isEmpty()) {
+                return ResultGenerator.genFailResult("上传第" + i + "个文件失败");
+            }
+            String fileName = UUID.randomUUID().toString() + originalName.substring(originalName.lastIndexOf("."));
+            File dest = new File(path, fileName);
+            try {
+                file.transferTo(dest);
+                temp += HeartBeatUtils.getHost(new URI(req.getRequestURL() + "")) + "/upload/" + fileName + ",";
+            } catch (IOException | URISyntaxException e) {
+                e.printStackTrace();
+                return ResultGenerator.genFailResult("上传第" + (i + 1) + "文件失败");
+            }
+        }
+        return ResultGenerator.genSuccessResultData(temp.substring(0, temp.length() - 1));
+    }
+
+//    @PostMapping("file")
+//    public Map<String, Object> fileUpload(MultipartFile file, HttpServletRequest req) {
+//        Map<String, Object> result = new HashMap<>();
+//        String originalFilename = file.getOriginalFilename();
+//        if (!originalFilename.endsWith(".png")) {
+//            result.put("status", "error");
+//            result.put("msg", "文件类型需为png结尾");
+//            return result;
+//        }
+//        String format = sdf.format(new Date());
+//        String realpath = req.getServletContext().getRealPath("/") + format;
+//        File folder = new File(realpath);
+//        if(!folder.exists()){
+//            folder.mkdir();
+//        }
+//        String newName = UUID.randomUUID().toString() + ".png";
+//        try {
+//            file.transferTo(new File(folder,newName));
+//            String url = req.getScheme() + "://" + req.getServerName() + ":" + req.getServerPort() + format + newName;
+//            result.put("status","success");
+//            result.put("url",url);
+//        } catch (IOException e) {
+//            result.put("status", "error");
+//            result.put("msg", e.getMessage());
+//        }
+//        return result;
+//    }
 }
---
+++ src/main/java/com/example/heartbeatadminserver/util/HeartBeatUtils.java
@@ -0,0 +1,15 @@
+package com.example.heartbeatadminserver.util;
+
+import java.net.URI;
+
+public class HeartBeatUtils {
+    public static URI getHost(URI uri) {
+        URI effectiveURI = null;
+        try {
+            effectiveURI = new URI(uri.getScheme(), uri.getUserInfo(), uri.getHost(), uri.getPort(), null, null, null);
+        } catch (Throwable var4) {
+            effectiveURI = null;
+        }
+        return effectiveURI;
+    }
+}
