e078f1a1f9ab7766ec047f24bd9b17ef44de847d
yy <jenniferyue@outlook.com>
2022-07-09 19:59:39 +0800

    yy: bug-fix 新增优惠券接口starttime和endtime字段前端传值只需要年月日，不需要时分秒

--- src/main/java/com/example/heartbeatadminserver/entity/Coupon.java
+++ src/main/java/com/example/heartbeatadminserver/entity/Coupon.java
@@ -42,15 +42,15 @@ public class Coupon implements Serializable {
     /**
      * 优惠券生效时间
      */
-    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @JsonFormat(pattern = "yyyy-MM-dd", timezone = "GMT+8")
     private Date starttime;
 
     /**
      * 优惠券失效时间
      */
-    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @JsonFormat(pattern = "yyyy-MM-dd", timezone = "GMT+8")
     private Date endtime;
 
     /**
