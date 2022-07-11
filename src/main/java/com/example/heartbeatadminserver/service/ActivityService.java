cf7924a88316f2fd3a25d60d3e22ea27030ee330
lhb <929580807@qq.com>
2022-07-11 16:07:02 +0800

    活动接口完成

---
+++ src/main/java/com/example/heartbeatadminserver/service/ActivityService.java
@@ -0,0 +1,13 @@
+package com.example.heartbeatadminserver.service;
+
+import com.example.heartbeatadminserver.entity.Activity;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+* @author lihanbin
+* @description 针对表【activity】的数据库操作Service
+* @createDate 2022-07-11 11:57:45
+*/
+public interface ActivityService extends IService<Activity> {
+
+}
